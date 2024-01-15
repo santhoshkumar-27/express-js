287d9acfa29a6a823a454092a2ea267aa525e0f8
santhoshkumar <its.santhoshkumar@outlook.com>
2024-01-15 12:47:27 +0530

    feat: write head and end

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("running server")
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    // console.log(req)
+
+    res.writeHead(200, {
+        "content-type" : 'text/html'
+    })
+    res.end('<h2> hello world </h2>')
+});
+
+
+server.listen(5000)
