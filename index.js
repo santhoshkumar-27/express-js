f61b38a9e492b32a1bcc18cf444febe805099d9f
santhoshkumar <its.santhoshkumar@outlook.com>
2024-01-15 13:06:52 +0530

    fix: normal http module

--- index.js
+++ index.js
@@ -3,11 +3,25 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     // console.log(req)
 
-    res.writeHead(200, {
-        "content-type" : 'text/html'
-    })
-    res.write('<h2> hello world </h2>')
-    res.end()
+    if (req.url === '/') {
+        res.writeHead(200, {
+            "content-type": 'text/html'
+        })
+        res.write('<h2> hello world </h2>')
+        res.end()
+    } else if (req.url === '/about') {
+        res.writeHead(200, {
+            "content-type": 'text/html'
+        })
+        res.write('<h2> about </h2>')
+        res.end()
+    } else {
+        res.writeHead(404, {
+            "content-type": 'text/html'
+        })
+        res.write('<h2> not found </h2>')
+        res.end()
+    }
 });
 
 
