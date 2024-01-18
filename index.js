5ae01674dc770aebea261d1308de12320a3e2b80
santhoshkumar <its.santhoshkumar@outlook.com>
2024-01-18 08:00:56 +0530

    feat: added mock data and serve the data

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const data = require('./mock-data/data')
+const app = express();
+const port = 5000;
+
+
+app.get('/', (req, res) => {
+    // res.json(data)
+    res.send(`
+        <h1>header</h1>
+        <a href='/api/data'>data</a>
+    `)
+})
+
+app.all('*', (req, res) => {
+    res.writeHead(404, {
+        "content-type": 'text/html'
+    })
+    res.write('<h2> not found </h2>')
+    res.end()
+})
+
+app.listen(port, () => {
+    console.log(`server is running on port http://localhost:${port}`)
+})
