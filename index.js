5a80609f8a1c0012d35dec83e4bd73ecad67119f
santhoshkumar <its.santhoshkumar@outlook.com>
2024-01-20 21:07:03 +0530

    feat: read data by id

--- index.js
+++ index.js
@@ -1,47 +1,30 @@
 const express = require('express');
 const data = require('./mock-data/data')
-const morgan = require('morgan')
-const { logger, authenticate } = require('./middleware')
 const app = express();
 const port = 5000;
 
-// req => middleware => res
+// read data
+app.get('/api/data', (req, res) => {
 
-// 1. use vs route
-// 2. options - our own / express / third party
+    res.json(
+        {
+            data
+        }
+    )
+})
+// read data by id
+app.get('/api/data/:id', (req, res) => {
+    const { id: requestedId } = req.params
+    const singleData = data.find((list) => list.id == requestedId);
+    if (singleData) {
+        res.json({ data: singleData });
+    } else {
+        res.json({
+            message: 'not found'
+        })
+    }
+})
 
-// app.use('/api', logger) // for specific route start path apply for middle wares
-// order is matter to excuted
-// app.use([logger, authenticate])
-
-app.use(morgan('tiny'))
-
-app.get('/', (req, res) => {
-    // res.json(data)
-    // console.log(req.method, req.url, new Date().getFullYear()); // need to console every incoming req
-    res.send(`
-        <h1>header</h1>
-    `)
-})
-app.get('/api/view', (req, res) => {
-    // res.json(data)
-    res.send(`
-        <h1>About</h1>
-    `)
-})
-app.get('/api/data', [authenticate] ,(req, res) => {
-    // res.json(data)
-    console.log(req.user)
-    res.send(`
-        <h1>data</h1>
-    `)
-})
-app.get('/about', (req, res) => {
-    // res.json(data)
-    res.send(`
-        <h1>About</h1>
-    `)
-})
 app.all('*', (req, res) => {
     res.writeHead(404, {
         "content-type": 'text/html'
