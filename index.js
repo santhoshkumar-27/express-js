c8343866504ff5911c3087cb5fe58fd9a7f7372e
santhoshkumar <its.santhoshkumar@outlook.com>
2024-01-20 08:52:08 +0530

    feat: middleware started

--- index.js
+++ index.js
@@ -3,7 +3,27 @@ const data = require('./mock-data/data')
 const app = express();
 const port = 5000;
 
+app.get('/', (req, res) => {
+    // res.json(data)
+    console.log(req.method, req.url, new Date().getFullYear())
+    res.send(`
+        <h1>header</h1>
+    `)
+})
+app.get('/about', (req, res) => {
+    // res.json(data)
+    res.send(`
+        <h1>About</h1>
+    `)
+})
 
+app.all('*', (req, res) => {
+    res.writeHead(404, {
+        "content-type": 'text/html'
+    })
+    res.write('<h2> not found </h2>')
+    res.end()
+})
 app.listen(port, () => {
     console.log(`server is running on port http://localhost:${port}`)
 })
