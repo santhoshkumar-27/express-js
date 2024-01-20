d63a04e4be9701675ef32c9b8f4799bfd34f022e
santhoshkumar <its.santhoshkumar@outlook.com>
2024-01-20 22:38:01 +0530

    fix: added body parser and post method

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require('express');
-const data = require('./mock-data/data')
+let data = require('./mock-data/data');
+const path = require('path')
 const app = express();
 const port = 5000;
+const bodyParser = require('body-parser')
+// static assets
+
+app.use('/', express.static(path.join(__dirname, '/public')))
+
+// app.use(express.urlencoded({extended: false}))
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 // read data
 app.get('/api/data', (req, res) => {
@@ -24,6 +37,46 @@ app.get('/api/data/:id', (req, res) => {
         })
     }
 })
+
+// read data by id
+app.get('/api/data/:id', (req, res) => {
+    const { id: requestedId } = req.params
+    const singleData = data.find((list) => list.id == requestedId);
+    if (singleData) {
+        res.json({ data: singleData });
+    } else {
+        res.json({
+            message: 'not found'
+        })
+    }
+})
+
+// create a entry data
+app.post('/api/data', (req, res) => {
+    const { title, body } = req.body;
+
+    if (!title || !body) {
+        return res.status(500).json({
+            message: 'failed'
+        })
+        
+    }
+    const {id,userId } = data[data.length - 1]
+    const preparedArray =  {
+        title, body,
+        id: id + 1,
+        userId
+    };
+    if (preparedArray) {
+        data.push(preparedArray)
+        res.json({ data: preparedArray });
+    } else {
+        res.status(500).json({
+            message: 'failed'
+        })
+    }
+})
+
 
 app.all('*', (req, res) => {
     res.writeHead(404, {
