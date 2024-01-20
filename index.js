15da138df9aed1f0cf4df69a6e889a8bc3eefdf7
santhoshkumar <its.santhoshkumar@outlook.com>
2024-01-20 09:04:04 +0530

    feat: sending response in the middlware

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const port = 5000;
 
 const logger = (req, res, next) => {
     console.log(req.method, req.url, new Date().getFullYear());
+    // console.log(next)
+    res.send('testing') // unless you send the response, we need to pass to the next middle ware
 }
 
 app.get('/', logger ,(req, res) => {
